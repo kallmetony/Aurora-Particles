ace638ca5412c8d6bdd595dfa75b117cd73d108d
Anton Ruzanov <antonru924@gmail.com>
2023-01-30 19:03:05 +0300

    README.md created. Bug fixed

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Aurora-Particles
-#### Lightweight purpur plugin.   
+#### Lightweight plugin.
 Very simple particle spawning using config file. All you have to do is to specify particles type and location, and it will be spawned automatically
-# Add particles
-1. Navigate to plugins config.yml  
-2. You must specify `particle, period, x, y, z, count` values for particle to create:  
-* `particle` is value from [enum of particles](https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Particle.html)   
-* `period` (default = 20) is ticks. Each `period` particle will be spawned again   
-* `x`, `y`, `z` is a coordinates   
-* `offset` (default = 0 for all offsets) particles will be spawned inside offset area      
-* `size` (default = 1) the size of a particle   
+## Add particles
+1. Navigate to plugins `config.yml`
+2. You must specify `particle, period, x, y, z, count` values for particle to create:
+* `particle` is value from [enum of particles](https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Particle.html)
+* `period` (default = 20) is ticks. Each `period` particle will be spawned again
+* `x`, `y`, `z` is a coordinates
+* `offset` (default = 0 for all offsets) particles will be spawned inside offset area
+* `size` (default = 1) the size of a particle
 #### Note that `offset`, `period` and `size` is optional to write
 ### Pattern for adding particles
 ```yaml
@@ -24,3 +24,7 @@ Very simple particle spawning using config file. All you have to do is to specif
   offsetZ: #offset
   size: #size of one particle
 ```
+## Setting up
+1. Download plugin (paper or purpur) from releases
+2. Write your values in `config.yml`
+3. You are ready to go, launch your server
--- src/main/java/com/aaronr92/auroraproject/handler/ParticleLoader.java
+++ src/main/java/com/aaronr92/auroraproject/handler/ParticleLoader.java
@@ -5,6 +5,8 @@ import com.aaronr92.auroraproject.model.Particle;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,6 +14,7 @@ import java.util.List;
 
 public class ParticleLoader {
 
+    private final Logger log = LoggerFactory.getLogger(ParticleLoader.class);
     private static final ParticleLoader instance = new ParticleLoader();
     private static final List<Particle> particles = Collections.synchronizedList(new ArrayList<>());
 
@@ -22,25 +25,29 @@ public class ParticleLoader {
     public void init() {
         ConfigurationSection configuration = Plugin.getPlugin().getConfig();
         configuration.getKeys(false).forEach(p -> {
-            Particle particle = new Particle(
-                    org.bukkit.Particle.valueOf(
-                            configuration.getString(p + ".particle")
-                    ),
-                    configuration.getDouble(p + ".x"),
-                    configuration.getDouble(p + ".y"),
-                    configuration.getDouble(p + ".z"),
-                    configuration.getInt(p + ".count"),
-                    configuration.getDouble(p + ".offsetX"),
-                    configuration.getDouble(p + ".offsetY"),
-                    configuration.getDouble(p + ".offsetZ"),
-                    configuration.getDouble(p + ".size")
-            );
-            particles.add(particle);
-            long period = configuration.getLong(p + ".period");
-            setupTask(
-                    particle,
-                    period == 0 ? 20L : period
-            );
+            try {
+                Particle particle = new Particle(
+                        org.bukkit.Particle.valueOf(
+                                configuration.getString(p + ".particle")
+                        ),
+                        configuration.getDouble(p + ".x"),
+                        configuration.getDouble(p + ".y"),
+                        configuration.getDouble(p + ".z"),
+                        configuration.getInt(p + ".count"),
+                        configuration.getDouble(p + ".offsetX"),
+                        configuration.getDouble(p + ".offsetY"),
+                        configuration.getDouble(p + ".offsetZ"),
+                        configuration.getDouble(p + ".size")
+                );
+                particles.add(particle);
+                long period = configuration.getLong(p + ".period");
+                setupTask(
+                        particle,
+                        period == 0 ? 20L : period
+                );
+            } catch (IllegalArgumentException e) {
+                log.warn("Particle is not loaded correctly. Check your config.yml!");
+            }
         });
     }
 
