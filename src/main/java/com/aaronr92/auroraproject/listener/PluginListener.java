3e43f066b04772ce68b7f9df524ace39f8c9a0b5
Anton Ruzanov <antonru924@gmail.com>
2023-01-31 17:40:46 +0300

    Removed unnecessary event listener

--- src/main/java/com/aaronr92/auroraproject/listener/PluginListener.java
+++ src/main/java/com/aaronr92/auroraproject/listener/PluginListener.java
@@ -1,12 +1,9 @@
 package com.aaronr92.auroraproject.listener;
 
 import com.aaronr92.auroraproject.handler.ParticleLoader;
-import org.bukkit.Location;
-import org.bukkit.Particle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PluginListener implements Listener {
     private final ParticleLoader particleLoader = ParticleLoader.getInstance();
@@ -14,19 +11,5 @@ public class PluginListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         particleLoader.sendParticlesToPlayer(event.getPlayer());
-    }
-
-    @EventHandler
-    public void onMove(PlayerMoveEvent event) {
-        Location loc = event.getPlayer().getLocation();
-        particleLoader.sendParticleToAllPlayers(
-                new com.aaronr92.auroraproject.model.Particle(
-                        Particle.CLOUD,
-                        loc.getBlockX(),
-                        loc.getY(),
-                        loc.getZ(),
-                        1
-                )
-        );
     }
 }
